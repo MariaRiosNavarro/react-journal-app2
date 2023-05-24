f6ff097cdcbf6329bdd9832875ae6d25e05daf4b
Maria Rios Navarro <propscode254@gmail.com>
2023-05-24 17:29:23 +0200

    remove useState for useLocalStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+// import { useState } from "react";
 import "./App.css";
 import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
@@ -6,39 +7,45 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { uid } from "uid";
 
-const initialEntries = [
-  {
-    id: 1000,
-    date: "Feb 5, 2025",
-    motto: "We are in a state of chaos",
-    notes:
-      "Today I learned about React State. It was fun! I can't wait to learn more.",
-  },
-  {
-    id: 999,
-    date: "Feb 4, 2025",
-    motto: "Props, Props, Props",
-    notes:
-      "Today I learned about React Props. Mad props to everyone who understands this!",
-  },
-  {
-    id: 998,
-    date: "Feb 3, 2025",
-    motto: "How to nest components online fast",
-    notes:
-      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
-  },
-  {
-    id: 997,
-    date: "Feb 2, 2025",
-    motto: "I'm a React Developer",
-    notes: "My React-ion when I learned about React: 😍",
-  },
-];
+// const initialEntries = [
+//   {
+//     id: 1000,
+//     date: "Feb 5, 2025",
+//     motto: "We are in a state of chaos",
+//     notes:
+//       "Today I learned about React State. It was fun! I can't wait to learn more.",
+//   },
+//   {
+//     id: 999,
+//     date: "Feb 4, 2025",
+//     motto: "Props, Props, Props",
+//     notes:
+//       "Today I learned about React Props. Mad props to everyone who understands this!",
+//   },
+//   {
+//     id: 998,
+//     date: "Feb 3, 2025",
+//     motto: "How to nest components online fast",
+//     notes:
+//       "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+//   },
+//   {
+//     id: 997,
+//     date: "Feb 2, 2025",
+//     motto: "I'm a React Developer",
+//     notes: "My React-ion when I learned about React: 😍",
+//   },
+// ];
+
+// Original was:
+//  const [entries, setEntries] = useState(initialEntries);
+// const [filter, setFilter] = useState("all"); // "all" or "favorites"
 
 function App() {
-  const [entries, setEntries] = useState(initialEntries);
-  const [filter, setFilter] = useState("all"); // "all" or "favorites"
+  const [entries, setEntries] = useLocalStorageState("entries", {
+    defaultValue: [],
+  });
+  const [filter, setFilter] = useLocalStorageState("all", { defaultValue: [] }); // "all" or "favorites"
 
   function handleAddEntry(newEntry) {
     const date = new Date().toLocaleDateString("en-us", {
